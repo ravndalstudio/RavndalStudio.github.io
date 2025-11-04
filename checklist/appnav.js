07d624a8cdbbee430c136a912b2830660928ab58
Atle Ravndal <aravndal@gmail.com>
2025-11-04 17:25:41 +0100

    Restrukturert info checklist app

--- checklist/appnav.js
+++ checklist/appnav.js
@@ -3,9 +3,10 @@ function loadAppNavigation() {
     const navHTML = `
         <nav id="appnav">
             <ul class="links">
-                <li><a href="checklist.html">Checklist app</a></li>
-                <li><a href="privacy.html">Privacy Policy</a></li>
-                <li><a href="privacy-en.html">Privacy Policy (EN)</a></li>
+                <li><a href="index.html">Checklist app</a></li>
+                <li><a href="privacy-no.html">Personvern</a></li>
+                <li><a href="terms-no.html">Vilkår for bruk (EULA, NO)</a></li>
+                <li><a href="privacy.html">Privacy Policy (EN)</a></li>
                 <li><a href="terms.html">Terms of Use (EULA)</a></li>
             </ul>
         </nav>
@@ -23,5 +24,31 @@ function loadAppNavigation() {
             }
         });
     }
+    // Add custom style to reduce top spacing (stronger override)
+    const style = document.createElement('style');
+    style.innerHTML = `
+        /* Checklist app: Remove excessive top whitespace */
+        #header {
+            margin-top: 50px !important;
+            padding-bottom: 0 !important;
+            height: 3rem !important;
+        }
+        @media screen and (max-width: 980px) {
+            #header {
+                height: 2rem !important;
+                padding-bottom: 0 !important;
+            }
+        }
+        #main > * {
+            padding-top: 1.5rem !important;
+        }
+        .post.featured {
+            margin-top: 0 !important;
+        }
+        #wrapper {
+            padding-top: 0 !important;
+        }
+    `;
+    document.head.appendChild(style);
 }
 document.addEventListener('DOMContentLoaded', loadAppNavigation);
